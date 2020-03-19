d3adae25148776216cdefe4ace18e3140f3425f8
Adauto <dadorpm@hotmail.com>
2020-03-19 16:41:48 -0300

    rotas

---
+++ cadastro-produto/src/views/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Home(){
+    return(
+        <div className="jumbotron">
+            <h1 className="display-3">Bem Vindo!</h1>
+            <p className="lead">Utilize bem seu sistema através da barra de navegação.</p>
+            <hr className="my-4"/>
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+            </p>
+        </div>
+    )
+}
+export default Home;
---
+++ cadastro-produto/src/views/produtos/cadastro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class CadastroProduto extends React.Component{
+    render(){
+        return(
+            <div className="card">
+                <div className="card-header">
+                    <h4 class="card-title">Cadastro Produto</h4>
+                </div>
+                <div className="card-body">
+
+                </div>
+            </div>
+        )
+    }
+}
